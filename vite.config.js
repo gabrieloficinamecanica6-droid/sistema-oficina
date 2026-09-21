f8a7e340d0a7c4093af81c9e337f0a6095237fac
gabrieloficinamecanica6-droid <gabrieloficinamecanica6@gmail.com>
2026-09-21 11:08:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -48,6 +48,8 @@ export default defineConfig({
       },
 
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        
         globPatterns: [
           '**/*.{js,css,html,svg,png,ico}'
         ],
